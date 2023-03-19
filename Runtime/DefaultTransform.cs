0f82cbb2dc4305b40e5f184383ea75a2a4b3142a
baba-s <shotababa1206@gmail.com>
2023-03-19 20:37:31 +0900

    fix

--- Runtime/DefaultTransform.cs
+++ Runtime/DefaultTransform.cs
@@ -10,15 +10,80 @@ namespace Kogane
     public sealed class DefaultTransform : MonoBehaviour
     {
         //================================================================================
+        // 変数
+        //================================================================================
+        private Vector3    m_defaultPosition;
+        private Vector3    m_defaultLocalPosition;
+        private Quaternion m_defaultRotation;
+        private Vector3    m_defaultEulerAngles;
+        private Vector3    m_defaultLocalEulerAngles;
+        private Vector3    m_defaultLocalScale;
+        private bool       m_isInitialize;
+
+        //================================================================================
         // プロパティ
         //================================================================================
-        public Vector3    DefaultPosition         { get; private set; }
-        public Vector3    DefaultLocalPosition    { get; private set; }
-        public Quaternion DefaultRotation         { get; private set; }
-        public Quaternion DefaultLocalRotation    { get; private set; }
-        public Vector3    DefaultEulerAngles      { get; private set; }
-        public Vector3    DefaultLocalEulerAngles { get; private set; }
-        public Vector3    DefaultLocalScale       { get; private set; }
+        public Vector3 DefaultPosition
+        {
+            get => m_defaultPosition;
+            private set
+            {
+                Initialize();
+                m_defaultPosition = value;
+            }
+        }
+
+        public Vector3 DefaultLocalPosition
+        {
+            get => m_defaultLocalPosition;
+            private set
+            {
+                Initialize();
+                m_defaultLocalPosition = value;
+            }
+        }
+
+        public Quaternion DefaultRotation
+        {
+            get => m_defaultRotation;
+            private set
+            {
+                Initialize();
+                m_defaultRotation = value;
+            }
+        }
+
+        public Quaternion DefaultLocalRotation { get; private set; }
+
+        public Vector3 DefaultEulerAngles
+        {
+            get => m_defaultEulerAngles;
+            private set
+            {
+                Initialize();
+                m_defaultEulerAngles = value;
+            }
+        }
+
+        public Vector3 DefaultLocalEulerAngles
+        {
+            get => m_defaultLocalEulerAngles;
+            private set
+            {
+                Initialize();
+                m_defaultLocalEulerAngles = value;
+            }
+        }
+
+        public Vector3 DefaultLocalScale
+        {
+            get => m_defaultLocalScale;
+            private set
+            {
+                Initialize();
+                m_defaultLocalScale = value;
+            }
+        }
 
         //================================================================================
         // 関数
@@ -28,6 +93,17 @@ namespace Kogane
         /// </summary>
         private void Awake()
         {
+            Initialize();
+        }
+
+        /// <summary>
+        /// 初期化します
+        /// </summary>
+        private void Initialize()
+        {
+            if ( m_isInitialize ) return;
+            m_isInitialize = true;
+
             var t = transform;
 
             DefaultPosition         = t.position;
